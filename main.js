bf8247e1acf2cde96740683f68c97fdea2e9390f
Rale <rastko.dragic@gmail.com>
2025-04-08 01:56:40 +0200

    merge

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const { request } = require('http');
 const { response } = require('express');
@@ -17,16 +16,4 @@ app.get('/search', (request, response) =>
 });
 
 
-=======
-const express = require('express')
-const { request } = require('http');
-const { response } = require('express');
-
-const app = express()
-
-app.set("view engine", "ejs");
-
-app.use(express.static('./client/'));
-
->>>>>>> e8fe9913ceb0243f46f5b6074a444fff1359c161
 app.listen(process.env.PORT || 8080, () => console.log("Running on port 8080"));
