03f69f9e7a9ab2df04ee65ea1429be52f515a3bb
Rale <rastko.dragic@gmail.com>
2025-04-08 01:49:53 +0200

    added routing for search

--- main.js
+++ main.js
@@ -8,5 +8,12 @@ app.set("view engine", "ejs");
 
 app.use(express.static('./client/'));
 
+//serch routing 
+//example search url: url/search?q=this+is+a+search
+app.get('/search', (request, response) => 
+{
+    const sQuery = req.query.q; 
+});
+
 
 app.listen(process.env.PORT || 8080, () => console.log("Running on port 8080"));
