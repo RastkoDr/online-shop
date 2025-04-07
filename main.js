995299cdd034d73cdf40c5eb83ce144a0fed1a29
Rale <rastko.dragic@gmail.com>
2025-04-07 22:21:33 +0200

    added the basic structure of the app

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const { request } = require('http');
+const { response } = require('express');
+
+const app = express()
+
+app.set("view engine", "ejs");
+
+app.use(express.static('./client/'));
+
+
+app.listen(process.env.PORT || 8080, () => console.log("Running on port 8080"));
