dddcb2a7a37e06c3ee2f4d52b927dbfdee69e2ca
Rale <rastko.dragic@gmail.com>
2025-04-08 02:27:44 +0200

    small fix to search

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
+const { urlencoded } = require('express');
 const express = require('express')
-const { request } = require('http');
-const { response } = require('express');
+//const { request } = require('http');
+//const { response } = require('express');
+
 
 const app = express()
 
 app.set("view engine", "ejs");
 
+app.use(urlencoded({extended: false}));
+
+
 app.use(express.static('./client/'));
+
+
+
 
 //serch routing 
 //example search url: url/search?q=this+is+a+search
-app.get('/search', (request, response) => 
+app.get('/search', async function (request, response) 
 {
-    const sQuery = req.query.q; 
+    const sQuery = request.query.q; 
+    console.log(sQuery);
+
+    //response.render(index);
+    response.send('send back json'); //todo
 });
 
 
